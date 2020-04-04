8acfb5e354028980ae6db0ad8b476a9851eff012
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 20:32:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,8 @@ def monopoly_with_fourth_tier
 	       :reading_railroad => {
 	         "mortgage_value" => "$100",
 	         "mortgage_value" => "$200",
-	         "mortgage_value"
+	         "mortgage_value" => "$300",
+	         "mortgage_value" => "400"
 	       },
 	       :pennsylvania_railroad => {},
 	       :b_and_o_railroad => {},
